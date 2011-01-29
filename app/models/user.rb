f964c3dc1c13cccf5baca73ebd50ead5f2b2062f
xdh <xdh@xdhdev.(none)>
2011-01-29 14:59:32 -0500

    New func getNextMatch() and spaceholder func generate_match()

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   has_many :matches2, :class_name => "Match", :foreign_key => 'match_id2'
   has_many :recommendations, :class_name => "Match", :foreign_key => 'recommender_id'
   has_and_belongs_to_many :likes
+
+  def getNextMatch(current_user) 
+    match = nil
+    match = Match.first
+    =>#<status: 0, recommender_id: current_user>
+    if match.nil? then
+        match = generate_matches(current_user)
+    else
+        match.status = "1"
+    end
+    return match
+  end
+
+  def generate_matches(current_user)
+    return nil
+  end
 end
+
+
