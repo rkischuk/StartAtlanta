f912d5d6af82bffc6764273efb641b459068411e
Travis Pew <travisp@gmail.com>
2011-01-30 13:22:18 -0500

    Add forgotton db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110130070726) do
+ActiveRecord::Schema.define(:version => 20110130174629) do
 
   create_table "facebooks", :force => true do |t|
     t.string   "identifier",   :limit => 20
