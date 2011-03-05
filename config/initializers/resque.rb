5290fc48a2cd711d2e457d354202000d414956db
root <root@ConnectMeWeb.(none)>
2011-03-05 05:14:30 +0000

    Fix config path

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+resque_config = YAML.load_file(rails_root + '/config/resque.yaml')
 Resque.redis = resque_config[rails_env]
